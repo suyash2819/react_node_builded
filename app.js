b45f0fd4e35818931ed683d2d014457a6bd00d29
suyash mandhana <mandhanasuyash@yahoo.in>
2020-12-22 23:54:05 +0530

    only one request from front end

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.get("/*", (req, res) => {
   res.sendFile("index.html", { root });
 });
 
-// catch 404 and forward to error handler
+//catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
--- routes/index.js
+++ routes/index.js
@@ -2,21 +2,37 @@ var express = require("express");
 var request = require("request");
 var router = express.Router();
 var path = require("path");
+const { rejects } = require("assert");
 var app = express();
 
 router.post("/", (req, res) => {
-  var rollnumber = req.body.rollnumber;
+  let rollnumbers = req.body.data;
 
-  request(
-    "http://proedge.me/test.php?rollnumber=" + rollnumber,
-    function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        res.send({ result: response.body, rollnumber: req.body.rollnumber }); // Show the HTML for the Google homepage.
-      } else {
-        console.log(error);
+  const promarray = rollnumbers.map((rollnumber, i) => {
+    return new Promise((resolve, reject) => {
+      if (isNaN(parseInt(rollnumber))) {
+        return reject(rollnumber + " is not valid Roll Number");
       }
-    }
-  );
+      request(
+        "http://proedge.me/test.php?rollnumber=" + rollnumber,
+        function (error, response, body) {
+          if (!error && response.statusCode == 200) {
+            return resolve({ result: response.body, rollnumber: rollnumber });
+          } else {
+            return reject(error);
+          }
+        }
+      );
+    });
+  });
+
+  Promise.allSettled(promarray)
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 module.exports = router;
